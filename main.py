66d28e97b1ac68312c6aaf4fbc4b5ac2f08d3352
K P Suhas <kpsuhas2703@gmail.com>
2026-06-20 14:28:02 +0530

    Video 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -24,3 +24,32 @@ async def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient Not Found")
+
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description = "Sort on the basis of height, weight or bmi"), order: str = Query('asc', desecription = "sort in asc or desc order")):
+
+    valid_feilds = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code = 400, detail = f'Invalid feild select from {valid_feilds}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code = 400, detail = 'Invalid order select from asc or desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key = lambda x : x.get(sort_by, 0), reverse = sort_order)
+
+    return sorted_data
